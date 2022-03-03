ac0798641e350538597abc8e867f7d5caf1a484b
Angel Valentin <angelgvalentin7@gmail.com>
2022-03-02 21:44:58 -0600

    Added notes

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,17 +2,29 @@ const express = require("express");
 const Logs = require("../models/Logs");
 const router = express.Router();
 
+/* -------------------------------------------------------------------------- */
+/*                               VIEW/GET ROUTE                               */
+/* -------------------------------------------------------------------------- */
+
 router.get("/", (req, res) => {
     Logs.find({}, (err, foundLogs) => {
         res.json(foundLogs);
     });
 });
 
+/* -------------------------------------------------------------------------- */
+/*                             CREATE / POST ROUTE                            */
+/* -------------------------------------------------------------------------- */
+
 router.post("/", (req, res) => {
     Logs.create(req.body, (err, createdLog) => {
         res.json(createdLog);
     });
 });
+
+/* -------------------------------------------------------------------------- */
+/*                               EDIT /PUT ROUTE                              */
+/* -------------------------------------------------------------------------- */
 
 router.put("/:id", (req, res) => {
     Logs.findByIdAndUpdate(
@@ -24,6 +36,10 @@ router.put("/:id", (req, res) => {
         }
     );
 });
+
+/* -------------------------------------------------------------------------- */
+/*                                DELETE ROUTE                                */
+/* -------------------------------------------------------------------------- */
 
 router.delete("/:id", (req, res) => {
     Logs.findByIdAndRemove(req.params.id, (err, deletedLog) => {
